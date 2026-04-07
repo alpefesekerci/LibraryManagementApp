32f79a8d0091ce82c762c0a7eb281fa6026779f4
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-04-07 10:51:21 +0300

    feat: implement repository layer for data access
    Created BookRepository to manage in-memory book collections and search by ISBN.
    
    Created MemberRepository to manage in-memory member collections and search by membership number.
    
    Removed PersonRepository as Person is an abstract class and does not require direct data access.

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -1,4 +1,31 @@
 package repository;
 
+import model.Book;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookRepository {
+    private List<Book> bookList;
+
+    public BookRepository() {
+        this.bookList = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        this.bookList.add(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookList;
+    }
+
+    public Book getBookByIsbn(String isbn) {
+        for (Book book : bookList) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
 }
--- src/repository/MemberRepository.java
+++ src/repository/MemberRepository.java
@@ -1,4 +1,30 @@
 package repository;
 
+import model.Member;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+    private List<Member> memberList;
+
+    public MemberRepository() {
+        this.memberList = new ArrayList<>();
+    }
+
+    public void addMember(Member member) {
+        this.memberList.add(member);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberList;
+    }
+
+    Member getMemberByMembershipNumber(String membershipNumber) {
+        for (Member member : memberList) {
+            if (member.getMembershipNumber().equals(membershipNumber)) {
+                return member;
+            }
+        }
+        return null;
+    }
 }
